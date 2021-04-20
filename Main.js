2df222aeb0b9140b48f9d3ee1311a0fb1eaf9079
mubeena <mubeena.ali@gmail.com>
2021-04-20 14:07:13 -0500

    adding main.js, input and search button working

---
+++ Main.js
@@ -0,0 +1,27 @@
+//let api_k = 'uTmMWqGKHFybvB8d8g7rUP32Yo7EbhxO';
+
+let s = document.getElementById("image-box");
+
+
+document.querySelector('#search_btn').addEventListener('click', () => {
+  let searchTerm = document.getElementById('search_item').value;
+ 
+
+let fetchPromise = fetch(`https://api.giphy.com/v1/gifs/search?api_key=uTmMWqGKHFybvB8d8g7rUP32Yo7EbhxO&q=${searchTerm}&limit=5&offset=0&rating=g&lang=en`);
+fetchPromise.then(response => {
+  return response.json();
+}).then(data => {
+
+console.log(data);
+
+document.getElementById('image-box').innerHTML = ""; 
+ data.data.forEach(val => {
+  let image_src =   val.images.fixed_width_downsampled.url;
+  let c1 = document.createElement("img");
+  c1.src = image_src;
+  s.appendChild(c1);
+ });
+
+
+})
+})
