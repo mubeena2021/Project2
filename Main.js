ba2f443224cf206703ef6318dc801a1b559b32b2
mubeena <mubeena.ali@gmail.com>
2021-04-20 22:15:19 -0500

    updated look and added number of gifs

--- Main.js
+++ Main.js
@@ -5,9 +5,10 @@ let s = document.getElementById("image-box");
 
 document.querySelector('#search_btn').addEventListener('click', () => {
   let searchTerm = document.getElementById('search_item').value;
- 
+  let num_limit = document.getElementById('displayNumber').value;
 
-let fetchPromise = fetch(`https://api.giphy.com/v1/gifs/search?api_key=uTmMWqGKHFybvB8d8g7rUP32Yo7EbhxO&q=${searchTerm}&limit=5&offset=0&rating=g&lang=en`);
+
+let fetchPromise = fetch(`https://api.giphy.com/v1/gifs/search?api_key=uTmMWqGKHFybvB8d8g7rUP32Yo7EbhxO&q=${searchTerm}&limit=${num_limit}&offset=0&rating=g&lang=en`);
 fetchPromise.then(response => {
   return response.json();
 }).then(data => {
@@ -16,7 +17,7 @@ console.log(data);
 
 document.getElementById('image-box').innerHTML = ""; 
  data.data.forEach(val => {
-  let image_src =   val.images.fixed_width_downsampled.url;
+  let image_src =  val.images.fixed_width_downsampled.url;
   let c1 = document.createElement("img");
   c1.src = image_src;
   s.appendChild(c1);
