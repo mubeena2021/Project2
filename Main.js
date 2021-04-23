8beb29fdf5fe6b2a476f014de47d4d2556ed3274
mubeena <mubeena.ali@gmail.com>
2021-04-22 21:02:18 -0500

    changed navbar colors

--- Main.js
+++ Main.js
@@ -1,14 +1,15 @@
-//let api_k = 'uTmMWqGKHFybvB8d8g7rUP32Yo7EbhxO';
 
-let s = document.getElementById("image-box");
+const apiKey = "uTmMWqGKHFybvB8d8g7rUP32Yo7EbhxO";
+let output = document.getElementById("image-box");
 
-
+window.addEventListener('load', () => {
+ 
 document.querySelector('#search_btn').addEventListener('click', () => {
   let searchTerm = document.getElementById('search_item').value;
   let num_limit = document.getElementById('displayNumber').value;
 
 
-let fetchPromise = fetch(`https://api.giphy.com/v1/gifs/search?api_key=uTmMWqGKHFybvB8d8g7rUP32Yo7EbhxO&q=${searchTerm}&limit=${num_limit}&offset=0&rating=g&lang=en`);
+let fetchPromise = fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchTerm}&limit=${num_limit}&offset=0&rating=g&lang=en`);
 fetchPromise.then(response => {
   return response.json();
 }).then(data => {
@@ -17,12 +18,12 @@ console.log(data);
 
 document.getElementById('image-box').innerHTML = ""; 
  data.data.forEach(val => {
-  let image_src =  val.images.fixed_width_downsampled.url;
+  let image_src =  val.images.fixed_width.url;
   let c1 = document.createElement("img");
+  c1.classList = "img-thumbnail rounded my-auto mx-auto ";
   c1.src = image_src;
-  s.appendChild(c1);
- });
-
-
-})
-})
+  output.appendChild(c1);
+     });
+   });
+  });
+});
